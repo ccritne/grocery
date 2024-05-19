70f4c289cbd48e15733fbcc22ff453b389afe8c5
ccritne <info.cristiancinieri@gmail.com>
2024-05-19 19:09:44 +0200

    feat: Open close ingredients and datetime changeable

--- app/src/main/java/com/example/testapp/Calendar.kt
+++ app/src/main/java/com/example/testapp/Calendar.kt
@@ -1,5 +1,7 @@
 package com.example.testapp
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -32,109 +34,10 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
-
-fun jsonStringToEasyMap(string: String) : Map<String, Int> {
-    /*
-        Easy Json
-        {
-            "key1": value1,
-            "key2": value2,
-            .
-            .
-            .
-            "keyN": valueN
-        }
-
-     */
-
-    var passedQMarks = false
-    var passedTwoPoints = false
-
-    var keyName = ""
-    var valueName = ""
-
-    val returnMap : MutableMap<String, Int> = emptyMap<String, Int>().toMutableMap()
-
-    for (i in string){
-        when (i) {
-            '\n' -> {}
-            ' ' -> {}
-            '{' -> {}
-            '}' -> {}
-            '"' -> passedQMarks = !passedQMarks
-            ':' -> passedTwoPoints = true
-            ',' -> {
-                    if(passedQMarks)
-                        keyName += i
-                    else {
-                        returnMap[keyName] = valueName.toInt()
-                        keyName = ""
-                        valueName = ""
-                        passedTwoPoints = false
-                    }
-                }
-            else -> {
-                valueName += if (!passedQMarks || passedTwoPoints) i else ""
-                keyName += if (passedQMarks && !passedTwoPoints) i else ""
-
-                /***
-                 * Quotation    TwoPoints   |   Result
-                 * False            False   |    Value
-                 * False            True    |    Value
-                 * True             False   |    Key
-                 * True             True    |    Value
-                 ***/
-            }
-        }
-    }
-
-    if(keyName != "" && valueName != "")
-        returnMap[keyName] = valueName.toInt()
-
-    return returnMap
-}
-
-fun findClosureBracket(string: String, index: Int): Int{
-
-    var openBracket = 1
-    var closeBracket = 0
-
-    var iterator = index
-
-    while (openBracket > closeBracket && iterator < string.length){
-        iterator++
-        when (string[iterator]) {
-            '{' -> openBracket++
-            '}' -> closeBracket++
-            else -> {}
-        }
-    }
-
-    return iterator
-
-}
-
-fun jsonStringToComplicatedMap(string: String) : Map<String, Any> {
-    var i = 0
-    var deep = 0
-    while( i < string.length){
-        when (string[i]) {
-            '{' -> {
-                val endClosureBracket = findClosureBracket(string, i)
-                var subString = string.substring(i, endClosureBracket)
-                i += endClosureBracket
-            }
-        }
-    }
-
-    return mapOf("Ciao" to "Pippo")
-}
-
-
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun Calendar() {
 
@@ -142,8 +45,8 @@ fun Calendar() {
         mutableStateOf(false)
     }
 
-    var foodDialog = remember{ mutableStateOf("") }
-    var gramsDialog  = remember{ mutableIntStateOf(0) }
+    val foodDialog = remember{ mutableStateOf("") }
+    val gramsDialog  = remember{ mutableIntStateOf(0) }
 
     @Composable
     fun Dialog(){
@@ -170,14 +73,18 @@ fun Calendar() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceAround
     ) {
-        val todayStr = SimpleDateFormat("d MMM y", Locale.getDefault()).format(Date())
+
+        val formatter = DateTimeFormatter.ofPattern("dd MMM y")
+        val date = remember { mutableStateOf(LocalDate.now())}
+        val formattedDate = date.value.format(formatter)
+
         Row(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically
 
         ) {
-            IconButton(onClick = { /*TODO*/ }) {
+            IconButton(onClick = { date.value = date.value.minusDays(1) }) {
                 Icon(
                     imageVector = Icons.Default.ArrowBack,
                     contentDescription = "backDate",
@@ -189,13 +96,13 @@ fun Calendar() {
                 colors = ButtonDefaults.textButtonColors(contentColor = Color.Black)
             ) {
                 Text(
-                    text = todayStr,
+                    text = formattedDate,
                     fontSize = 25.sp,
                     fontWeight = FontWeight.ExtraBold,
                     fontFamily = FontFamily.Monospace,
                 )
             }
-            IconButton(onClick = { /*TODO*/ }) {
+            IconButton(onClick = { date.value = date.value.plusDays(1) }) {
                 Icon(
                     imageVector = Icons.Default.ArrowForward,
                     contentDescription = "forwardDate",
@@ -214,40 +121,6 @@ fun Calendar() {
             verticalArrangement = Arrangement.SpaceAround,
         ) {
 
-            val jsonEatMoments = """
-                {
-                    "Breakfast": {
-                        "Milk":444,
-                        "Biscuit":254
-                    },
-                    "1° Snack": {
-                        "Apple": 804 
-                    },
-                    "Lunch":{
-                        "Pasta":717,
-                        "Tomato sauce":578,
-                        "Salad":889,
-                        "Carrots":610
-                    },
-                    "2° Snack":{
-                        "Yogurt":580,
-                        "Muesli":276
-                    },
-                    "Dinner":{
-                        "Pepper":652,
-                        "Oil":79,
-                        "Mozzarella":492
-                    },
-                    "3° Snack":{
-                        "Ice Cream":356,
-                        "Cocoa":507,
-                        "Smarties":695
-                    }
-                }
-            """.trimIndent()
-
-
-
             val mapEatMoments = mapOf(
                 "Breakfast" to mapOf("Milk" to 444, "Biscuits" to 254),
                 "1° Snack" to mapOf("Apple" to 804),
@@ -257,10 +130,59 @@ fun Calendar() {
                 "3° Snack" to mapOf("Ice Cream" to 356, "Cocoa" to 507, "Smarties" to 695),
             )
 
+            // Breakfast()
+            // FirstSnack()
+            // Lunch()
+            // SecondSnack()
+            // Dinner
+            // ThirdSnack()
 
-            mapEatMoments.forEach { moment ->
+            val subMenuShow = remember { mutableStateOf(false) }
+            val listIngredients = remember { mutableStateOf(mapOf<String, Int>()) }
+            val idSelectedShow = remember { mutableIntStateOf(-1) }
+
+            @Composable
+            fun Ingredients(moment: Map<String, Int>){
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 10.dp, end = 10.dp),
+                    horizontalAlignment = Alignment.Start,
+                    verticalArrangement = Arrangement.SpaceAround
+                ) {
+                    moment.forEach { food ->
+
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clickable {
+                                    dialogShow.value = true
+                                    foodDialog.value = food.key
+                                    gramsDialog.intValue = food.value
+                                },
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                        ) {
+                            Text(text = food.key, fontSize = 25.sp)
+                            Text(
+                                text = food.value.toString() + "g",
+                                fontSize = 25.sp
+                            )
+                        }
+
+                    }
+                }
+            }
+
+            @Composable
+            fun Moment(momentName: String, ingredients: Map<String, Int>, idSelected : Int){
                 Row(
-                    modifier = Modifier.fillMaxWidth(),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable {
+                            subMenuShow.value = !subMenuShow.value
+                            listIngredients.value = ingredients
+                            idSelectedShow.intValue = idSelected
+                        },
                     horizontalArrangement = Arrangement.Center
                 ) {
                     Column(
@@ -269,44 +191,28 @@ fun Calendar() {
                         verticalArrangement = Arrangement.SpaceAround
                     ) {
                         Text(
-                            text = moment.key.uppercase(),
+                            text = momentName.uppercase(),
                             modifier = Modifier.padding(10.dp),
                             fontSize = 35.sp,
                             fontWeight = FontWeight.ExtraBold,
                         )
-                        Column(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(start = 10.dp, end = 10.dp),
-                            horizontalAlignment = Alignment.Start,
-                            verticalArrangement = Arrangement.SpaceAround
-                        ) {
-                            moment.value.forEach { food ->
 
-                                Row(
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .clickable {
-                                            dialogShow.value = true
-                                            foodDialog.value = food.key
-                                            gramsDialog.intValue = food.value
-                                        },
-                                    horizontalArrangement = Arrangement.SpaceBetween,
-                                ) {
-                                    Text(text = food.key, fontSize = 25.sp)
-                                    Text(
-                                        text = food.value.toString() + "g",
-                                        fontSize = 25.sp
-                                    )
-                                }
+                        if(subMenuShow.value && idSelectedShow.intValue == idSelected)
+                            Ingredients(moment = ingredients)
 
-                            }
-                        }
 
                     }
 
                 }
             }
+
+
+            var iterator = 0
+            mapEatMoments.forEach { moment ->
+                Moment(momentName = moment.key, ingredients = moment.value, idSelected = iterator)
+                iterator++
+            }
+
 
 
         }
