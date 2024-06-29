fdc5a5893ca9fee22496a2aced2afcb4aa5bd782
ccritne <info.cristiancinieri@gmail.com>
2024-06-29 14:42:00 +0200

    delete unused imports

--- app/src/main/java/com/example/grocery/Navigation.kt
+++ app/src/main/java/com/example/grocery/Navigation.kt
@@ -1,16 +1,14 @@
 package com.example.grocery
 
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.grocery.body.Inventory
 import com.example.grocery.body.ListItems
-import com.example.grocery.body.ShoppingCart
-import com.example.grocery.body.UpdateItem
 import com.example.grocery.body.Plan
 import com.example.grocery.body.Profile
+import com.example.grocery.body.ShoppingCart
+import com.example.grocery.body.UpdateItem
 import com.example.grocery.utilities.Screen
 
 
